43d582f2754d254a7c2da57162ef6c0d4560363c
alexis <alesixvaldivia@gmail.com>
2025-11-11 20:51:33 -0300

    Implementando CORS

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import preinscripcionRouter from './modules/pre-inscripcion-form/preInscripcion.router.js';
 import uploadRouter from './modules/file-upload/upload.routes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
-app.use(uploadRouter)
+app.use(uploadRouter);
 app.use(express.json());
 app.use(preinscripcionRouter);
+app.use(
+	cors({
+		origin: '*',
+		methods: ['GET', 'PUT', 'PATCH', 'POST'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	})
+);
 
 app.set('port', process.env.PORT);
 

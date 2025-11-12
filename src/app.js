69e3d8cd7e8d990652fecbe3e0b81b63f4eda8b8
TomasQtr55 <quinterostomas2003@gmail.com>
2025-11-12 11:12:29 -0300

    solucion de cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 
 dotenv.config();
 
+const app = express();
 app.use(
 	cors({
 		origin: '*',
@@ -14,7 +15,6 @@ app.use(
 	})
 );
 
-const app = express();
 
 app.use(uploadRouter);
 app.use(express.json());
